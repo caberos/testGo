4d6a9d16f84c346850bd5895440d0fd1b96d4e60
caberos <cabermaldo010788@gmail.com>
2020-05-28 10:22:45 -0400

    new project

---
+++ Operation.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func sum(x, y int) int {
+	return x + y
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/caberos/testGo"
+)
+
+func main() {
+
+	fmt.Println(Operation.sum(5, 6))
+}
